89eee7f398511c926a3d2701459f8dc4d07f7cbe
Akmaljon <akmaljonsamandarov@gmail.com>
2026-03-15 16:39:09 +0500

    Complete task

--- mcp_server/tools/users/search_users_tool.py
+++ mcp_server/tools/users/search_users_tool.py
@@ -7,26 +7,40 @@ class SearchUsersTool(BaseUserServiceTool):
 
     @property
     def name(self) -> str:
-        #TODO: Provide tool name as `search_users`
-        raise NotImplementedError()
+        return 'search_users'
 
     @property
     def description(self) -> str:
-        #TODO: Provide description of this tool
-        raise NotImplementedError()
+        return 'Search users tool'
 
     @property
     def input_schema(self) -> dict[str, Any]:
-        #TODO:
-        # Provide tool params Schema:
-        # - name: str
-        # - surname: str
-        # - email: str
-        # - gender: str
-        # None of them are required (see UserClient.search_users method)
-        raise NotImplementedError()
+        return {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string",
+                    "description": "The name of the user to search"
+                },
+                "surname": {
+                    "type": "string",
+                    "description": "The surname of the user to search"
+                },
+                "email": {
+                    "type": "string",
+                    "description": "The email of the user to search"
+                },
+                "gender": {
+                    "type": "string",
+                    "description": "The gender of the user to search"
+                },
+
+            },
+            "required": []
+        }
 
     async def execute(self, arguments: dict[str, Any]) -> str:
-        #TODO:
-        # Call user_client search_users (with `**arguments`) and return its results (it is async, don't forget to await)
-        raise NotImplementedError()
+        try:
+            return await self._user_client.search_users(**arguments)
+        except Exception as e:
+            return f"Error while searching users: {str(e)}"
--- mcp_server/tools/users/update_user_tool.py
+++ mcp_server/tools/users/update_user_tool.py
@@ -8,26 +8,33 @@ class UpdateUserTool(BaseUserServiceTool):
 
     @property
     def name(self) -> str:
-        #TODO: Provide tool name as `update_user`
-        raise NotImplementedError()
+        return 'update_user'
 
     @property
     def description(self) -> str:
-        #TODO: Provide description of this tool
-        raise NotImplementedError()
+        return 'Update the user info'
 
     @property
     def input_schema(self) -> dict[str, Any]:
-        #TODO:
-        # Provide tool params Schema:
-        # - id: number, required, User ID that should be updated.
-        # - new_info: UserUpdate.model_json_schema()
-        raise NotImplementedError()
+        params = {
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "number",
+                    "description": "User ID that should be updated"
+                }
+            },
+            "required": [
+                "id"
+            ]
+        }
+        params['properties']['new_info'] = UserUpdate.model_json_schema()
+        return params
 
     def execute(self, arguments: dict[str, Any]) -> str:
-        #TODO:
-        # 1. Get user `id` from `arguments`
-        # 2. Get `new_info` from `arguments` and create `UserUpdate` via pydentic `UserUpdate.model_validate`
-        # 3. Call user_client update_user and return its results (it is async, don't forget to await)
-        raise NotImplementedError()
-
+        user_id = arguments['id']
+        try:
+            update_user = UserUpdate.model_validate(arguments['new_info'])
+            self._user_client.update_user(user_id, update_user)
+        except Exception as e:
+            return f"Error while updating user: {str(e)}"
